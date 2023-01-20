c9aa36c8651cf7bc027b0e7bf89bff6444a48857
onurravli <onurravli@hotmail.com>
2023-01-21 01:28:45 +0300

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup (
     name='turkishid',
-    version='0.1.1',
+    version='0.2',
     license='MIT',
     author="Onur Ravli",
     author_email='onur@ravli.co',
